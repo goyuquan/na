85ed0b8f9bf324f2b0e3bf2ef94abe21e5630c17
goyuquan <gyqzone@sina.com>
2016-04-27 09:03:06 +0000

    homepage 1.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -90,15 +90,64 @@
     </ul>
 </div>
 
+
 <div class="category container">
     <div class="head">
         <h2>Sell or Advertise anything using</h2>
     </div>
     <div class="column">
-        <h3>mobile phone</h3>
+        <h3><a href="#">老中青</a></h3>
         <ul>
             <li>
-                <a href="#">mobile phhone</a>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">温暖</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">噢民</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">党中央</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">一缺抽</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">的脱掉</a>
+            </li>
+        </ul>
+        <h3><a href="#">老中青</a></h3>
+        <ul>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">温暖</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">噢民</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">党中央</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">一缺抽</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">的脱掉</a>
             </li>
         </ul>
     </div>
@@ -106,7 +155,51 @@
         <h3>mobile phone</h3>
         <ul>
             <li>
-                <a href="#">mobile phhone</a>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+        </ul>
+        <h3><a href="#">老中青</a></h3>
+        <ul>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">胆固醇</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">温暖</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">噢民</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">党中央</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">一缺抽</a>
+            </li>
+            <li>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <a href="#">的脱掉</a>
             </li>
         </ul>
     </div>
@@ -135,4 +228,102 @@
         </ul>
     </div>
 </div>
+
+<h2 class="latest_title">The latest things 最新内容</h2>
+<div class="latest_list container">
+    <div class="content">
+        <dl>
+            <dt><a href="#">分类标题</a></dt>
+            <dd>
+                <span class="date">2016-5-1</span>
+                <h4><a href="#">The latest things 最新内容</a></h4>
+                <span class="price">面议</span>
+                <span class="phone">54632541256</span>
+                <p>
+                    Furnished room, all utilities included.
+                    Free wifi and cable.
+                    Off street parking.
+                    Kitchen privileges, includes dishes, pots  pans etc.
+                    Off street parking.
+                </p>
+            </dd>
+            <dd>
+                <span class="date">2016-5-1</span>
+                <h4><a href="#">The latest things 最新内容</a></h4>
+                <span class="price">￥35</span>
+                <span class="phone">54632541256</span>
+                <p>
+                    Furnished room, all utilities included.
+                    Free wifi and cable.
+                    Off street parking.
+                </p>
+            </dd>
+            <dd>
+                <span class="date">2016-5-1</span>
+                <h4><a href="#">The latest things 最新内容</a></h4>
+                <span class="price">￥35</span>
+                <span class="phone">54632541256</span>
+                <p>
+                    Furnished room, all utilities included.
+                    Free wifi and cable.
+                    Off street parking.
+                </p>
+            </dd>
+        </dl>
+        <dl>
+            <dt><a href="#">分类标题</a></dt>
+            <dd>
+                <span class="date">2016-5-1</span>
+                <h4><a href="#">The latest things 最新内容</a></h4>
+                <span class="price">面议</span>
+                <span class="phone">54632541256</span>
+                <p>
+                    Furnished room, all utilities included.
+                    Free wifi and cable.
+                    Off street parking.
+                    Kitchen privileges, includes dishes, pots  pans etc.
+                    Off street parking.
+                </p>
+            </dd>
+            <dd>
+                <span class="date">2016-5-1</span>
+                <h4><a href="#">The latest things 最新内容</a></h4>
+                <span class="price">￥35</span>
+                <span class="phone">54632541256</span>
+                <p>
+                    Furnished room, all utilities included.
+                    Free wifi and cable.
+                    Off street parking.
+                </p>
+            </dd>
+            <dd>
+                <span class="date">2016-5-1</span>
+                <h4><a href="#">The latest things 最新内容</a></h4>
+                <span class="price">￥35</span>
+                <span class="phone">54632541256</span>
+                <p>
+                    Furnished room, all utilities included.
+                    Free wifi and cable.
+                    Off street parking.
+                </p>
+            </dd>
+        </dl>
+
+    </div>
+    <div class="sidebar">
+        @include('includes.home_sidebar_pic_ad')
+
+        <h2 class="news_list"> <a href="#"> 首页新闻列表 </a> </h2>
+        <ul>
+            <li><a href="#">非法民不有压下夺有的</a></li>
+            <li><a href="#">oordinate representations</a></li>
+            <li><a href="#">oordinate representations</a></li>
+            <li><a href="#">oordinate representations</a></li>
+            <li><a href="#">oordinate representations</a></li>
+            <li><a href="#">oordinate representations</a></li>
+        </ul>
+
+    </div>
+</div>
+
 @endsection
---
+++ resources/views/includes/home_sidebar_pic_ad.blade.php
@@ -0,0 +1,1 @@
+home_sidebar_pic_ad
