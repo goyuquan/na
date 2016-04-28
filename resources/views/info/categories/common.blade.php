b9ed6386654a9d3c0ca31358922322801368b4a6
goyuquan <gyqzone@sina.com>
2016-04-28 10:09:39 +0000

    catertory style start

--- resources/views/info/categories/common.blade.php
+++ resources/views/info/categories/common.blade.php
@@ -4,18 +4,38 @@
 @section('description','title_description')
 @section('keywords','title_keywords')
 
-@section('content')
-<h1>全部类别 > {{$category->name}}</h1>
+@section('style')
+<link rel="stylesheet" href="{{url('/css/category.css')}}" >
+@endsection
 
-@if(count($infos) > 0)
-<ul>
-    @foreach( $infos as $info )
-        <li><a href="{{url('/info/'.$info->id)}}">{{ $info->title }}</a> </li>
-    @endforeach
-</ul>
-<p>
-</p>
-@endif
-{{$infos->links()}}
+@section('content')
+<div class="breadcrumb container">
+    <a href="{{url('/')}}">首页</a>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+    <a href="#">全部类别</a>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+    <span>{{$category->name}}</span>
+</div>
+
+<div class="main_wrap container">
+
+    <div class="category">
+        55555555555555555
+    </div>
+    <div class="content">
+        @if(count($infos) > 0)
+        <ul>
+            @foreach( $infos as $info )
+            <li><a href="{{url('/info/'.$info->id)}}">{{ $info->title }}</a> </li>
+            @endforeach
+        </ul>
+        <p>
+        </p>
+        @endif
+        {{$infos->links()}}
+
+    </div>
+
+</div>
 
 @endsection
